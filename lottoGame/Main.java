e97cf4a398d7668f28322fbaff49a1c2303f9722
machowina <k.machowina@gmail.com>
2017-12-08 13:20:06 +0100

    Merge branch 'master' of https://github.com/machowina/fantastic-octo-invention

--- lottoGame/Main.java
+++ lottoGame/Main.java
@@ -3,6 +3,8 @@
  */
 package lottoGame;
 
+import java.util.Scanner;
+
 /**
  * @author katarzyna
  *
@@ -14,7 +16,46 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
+	
+	static void lottoGame(){
+		//opening scanner
+		Scanner scan = new Scanner(System.in);
+		int[] givenNumbers = {0,0,0,0,0,0};
+		
+		for (int i = 0; i < givenNumbers.length; i++) {
+			
+			boolean keepGoing = true;
+			while (keepGoing){
+				
+				System.out.println("Podaj liczbę:");
+				//checking if it's an int
+				while (!scan.hasNextInt()){
+					System.out.println("To nie jest liczba!");
+					scan.next();
+				}
+				//reading the number from console
+				givenNumbers[i] = scan.nextInt();
+				keepGoing = false;
+				for (int j = 0; j < givenNumbers.length; j++) {
+					if (givenNumbers[i]==givenNumbers[j]){
+						keepGoing = true;
+					}
+				}
+				
+				if ((givenNumbers[i]>49)||(givenNumbers[i]<1)){
+					keepGoing = true;
+				}
+			}
+			
+		}
+		scan.close();
+		
+		
+	}
+	
+	
 
 }
