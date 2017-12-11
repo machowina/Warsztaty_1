82c255064674fb16e94f7c3af8ab9c7a3eae3f2d
machowina <k.machowina@gmail.com>
2017-12-11 19:25:13 +0100

    zadanie z kostką

--- diceGame/Main.java
+++ diceGame/Main.java
@@ -3,6 +3,10 @@
  */
 package diceGame;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * @author katarzyna
  *
@@ -13,16 +17,68 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println(diceRoll(diceType(getDiceType())));
 	}
+	/**
+	 * 
+	 * @return String - typ rzutu w stringu
+	 */
 	static String getDiceType () {
 		
-		
+		System.out.println("Podaj kod rzutu");
+		Scanner scan = new Scanner(System.in);
+		String str = "";
+		while (true) {
+			str = scan.nextLine();
+			//sprawdzanie czy jest podany prawidlowy typ kostki
+			if (str.contains("D3")||str.contains("D4")||str.contains("D6")||str.contains("D8")||str.contains("D10")
+					||str.contains("D12")||str.contains("D20")||str.contains("D100")){
+				break;
+			}	
+			System.out.println("To nie jest prawidłowy kod");
+		}
+		scan.close();
+		return str;
 	}
 	
+	/**
+	 * 
+	 * @param str - typ rzutu w Stringu
+	 * @return int[] - typ rzutu w tablicy 3 elem. int
+	 */
 	static int[] diceType (String str) {
 		
+		boolean minus = str.contains("-");
+		String[] textArr = str.split("\\D");
+		//poprawki po splicie na prawidlowy schemat
+		if (textArr[0].equals("")) {
+			textArr[0]="1";
+		}
+		if (minus){
+			textArr[textArr.length-1] = "-"+textArr[textArr.length-1];
+		}
+		if (textArr.length == 2){
+			textArr = Arrays.copyOf(textArr, 3);
+			textArr[2] = "0";
+		}
+		//ze stringa na int
+		int[] arr = new int[textArr.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(textArr[i]);
+		}
+		return arr;
 	}
+	/**
+	 * 
+	 * @param diceType - tablica int
+	 * @return int - wynik rzutu daną kostką
+	 */
+	static int diceRoll (int[] diceType){
+		//losowanie
+		Random r = new Random();
+		return (diceType[0]*(r.nextInt(diceType[1])+1)) +diceType[2];
+	}
+	
 
 }
--- searchingPopularWords/Main.java
+++ searchingPopularWords/Main.java
@@ -3,18 +3,86 @@
  */
 package searchingPopularWords;
 
-/**
- * @author katarzyna
- *
- */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 public class Main {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		getPopularWords();
 
+	    
 	}
+
+	static void findMostPopular(){
+		File file = new File("popular_words.txt");
+		StringBuilder reading = new StringBuilder();
+		try{
+			Scanner scan = new Scanner(file);
+	    	while (scan.hasNextLine()) {
+	    		reading.append(scan.nextLine()).append(" ");
+	    	}
+	    	scan.close();
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		StringTokenizer strTok = new StringTokenizer(reading.toString()," ");
+		int count = strTok.countTokens();
+		String[] arr = new String[count];
+		int i = 0;
+		while (strTok.hasMoreTokens()){
+			String str = strTok.nextToken();
+			if (str.length() > 3){
+				arr[i] = str;
+				i++;
+			}
+		}
+		Arrays.sort(arr);
+		
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j].equals(null)){
+				break;
+			
+		}
+		
+	}
+	
+	
+	static void getPopularWords(){
+		Connection connect = Jsoup.connect("https://www.onet.pl/");
+
+        try {
+
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            
+            FileWriter file = new FileWriter("popular_words.txt", true);
+            
+            for (Element elem : links) {
+                String[] strings = elem.text().split("[-.,\\s:\"!\\/?]");
+                for (int i = 0; i < strings.length; i++) {
+                	file.append(strings[i]).append(" ");
+				}  
+            }
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
 
 }
